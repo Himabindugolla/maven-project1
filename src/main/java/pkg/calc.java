ba4ca777a1fe47ddfed8d63705c7fbb547931c13
Hima Bindu Golla <himabindug2424@gmail.com>
2024-05-01 13:09:47 +0530

    diff is added

--- src/main/java/pkg/calc.java
+++ src/main/java/pkg/calc.java
@@ -5,11 +5,16 @@ public class calc {
 	{
 		return a+b;
 	}
+	public int diff(int a, int b)
+	{
+		return a-b;
+	}
 	
 
 	public static void main(String[] args) {
 		calc cal = new calc();
 	    System.out.println("sum is"+cal.sum(20,10));
+	    System.out.println("diff is"+cal.diff(20,10));
 		// TODO Auto-generated method stub
 
 	}
