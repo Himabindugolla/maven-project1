5edee350aff576d4b6cdb87060b43c4728cbe8cc
Hima Bindu Golla <himabindug2424@gmail.com>
2024-05-01 15:20:38 +0530

    mul is added

--- src/main/java/pkg/calc.java
+++ src/main/java/pkg/calc.java
@@ -9,12 +9,17 @@ public class calc {
 	{
 		return a-b;
 	}
+	public int mul(int a, int b)
+	{
+		return a*b;
+	}
 	
 
 	public static void main(String[] args) {
 		calc cal = new calc();
 	    System.out.println("sum is"+cal.sum(20,10));
 	    System.out.println("diff is"+cal.diff(20,10));
+	    System.out.println("mul is"+cal.mul(20,10));
 		// TODO Auto-generated method stub
 
 	}
