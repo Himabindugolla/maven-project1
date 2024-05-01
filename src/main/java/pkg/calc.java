7be89a80e293225669c52db72b9d743312578475
Hima Bindu Golla <himabindug2424@gmail.com>
2024-05-01 12:43:27 +0530

    maven project demo

---
+++ src/main/java/pkg/calc.java
@@ -0,0 +1,17 @@
+package pkg;
+
+public class calc {
+	public int sum(int a, int b)
+	{
+		return a+b;
+	}
+	
+
+	public static void main(String[] args) {
+		calc cal = new calc();
+	    System.out.println("sum is"+cal.sum(20,10));
+		// TODO Auto-generated method stub
+
+	}
+
+}
