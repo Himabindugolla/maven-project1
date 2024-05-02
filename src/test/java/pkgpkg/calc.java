8724983ffee960e6b3be5206b350fabd3c25bd6b
Hima Bindu Golla <himabindug2424@gmail.com>
2024-05-02 22:19:15 +0530

    junit file added

---
+++ src/test/java/pkgpkg/calc.java
@@ -0,0 +1,14 @@
+package pkgpkg;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class calc {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
